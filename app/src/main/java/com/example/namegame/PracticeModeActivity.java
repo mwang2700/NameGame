bf820a136d5ed65a316d1a65b4750da0c4da6293
maxwang2700 <maxwang2700@gmail.com>
2021-11-12 01:17:16 -0500

    Basic layout, integration with Firebase

---
+++ app/src/main/java/com/example/namegame/PracticeModeActivity.java
@@ -0,0 +1,14 @@
+package com.example.namegame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PracticeModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_practice_mode);
+    }
+}
