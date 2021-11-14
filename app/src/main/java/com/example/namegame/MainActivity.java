3e7fddb5367e9301cda199cc17222eecac38347f
maxwang2700 <maxwang2700@gmail.com>
2021-11-13 19:50:25 -0500

    Basic setup for activity + fragments for screens

--- app/src/main/java/com/example/namegame/MainActivity.java
+++ app/src/main/java/com/example/namegame/MainActivity.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class PracticeModeActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_practice_mode);
+        setContentView(R.layout.activity_main);
+
     }
 }
